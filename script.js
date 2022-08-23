0f1be8eee4d64e856b4e8c0a6252d5e4c152a008
eragon11145 <109441971+eragon11145@users.noreply.github.com>
2022-08-23 10:31:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,33 @@ function guess(word){
   }
 
 }
+var roots,definitions,x,y,word, word2
+roots=['epi','hyper','hypo','in','re','trans','un','con','in','chromo','dem','derm','feder','gen','meso','mono','pachy','pan','poly','scop','al','ant','ia','ic','ium','less','ly','ness','oid']
+definitions=['on, outside','over, above','under, below','not','back, again','across, through, over','not','with, together','not','color','people','skin','faith, trust','birth, produce, rate','middle','one','thick','all','many','look, view','like, related to','one who, that which','condition','like, related to','chemical element','without','like, manner of','state of','resembling']
+x=Math.floor(28*Math.random())
+y=Math.floor(28*Math.random())
+function L(){
+  x=Math.floor(28*Math.random())
+y=Math.floor(28*Math.random())
+  document.getElementById('root').innerHTML=definitions[x]
+}
+function guess(word){
+  word=word.trim()
+  word=word.toLowerCase();
+  word2=word[0]
+  word=word.slice(1)
+  word=word2+word
+  if (word==roots[x]){
+    document.getElementById("message").innerHTML='Correct! The root was ' + roots[x] +'!';
+  L()
+  document.getElementById("attempt").value=' '
+  }
+  
+
+}
+var input = document.getElementById("attempt")
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+  guess(document.getElementById("attempt").value)
+}
+})
